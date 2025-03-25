de1f716c26167056edce0e88eb19c8e2c41388bd
mykelweaver21 <mykelweaver21@gmail.com>
2025-03-25 03:22:22 -0400

    backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "aws-sucks-but-i-like-terraform"
-    region = "us-east-2"
-    dynamodb_table = "Terraform-Lock"
-    key = "week10/terraform.tfstate"
+    bucket = ""
+    region = ""
+    dynamodb_table = ""
+    key = ""
     encrypt = true
   }
 }
